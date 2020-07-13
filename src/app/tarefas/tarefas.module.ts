4c822247ec8f4c856dd722b65f58843357907f18
Natalia Uyu <nasilva.java@gmail.com>
2020-07-13 14:17:47 -0300

    Configurando rotas de cadastro de tarefas

--- src/app/tarefas/tarefas.module.ts
+++ src/app/tarefas/tarefas.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-
 import { TarefaService } from './shared';
 import { ListarTarefaComponent } from './listar';
+import { CadastrarTarefaComponent } from './cadastrar';
 
 
 
 @NgModule({
   declarations: [
-  ListarTarefaComponent
+  ListarTarefaComponent,
+  CadastrarTarefaComponent
   ],
 
   imports: [
