005e4a25e30b63a390c77ec9f35a1c64fabce5c4
Natalia Uyu <nasilva.java@gmail.com>
2020-07-14 08:41:44 -0300

    Excluir tarefas finalizado, implementando directiva tarefa concluída

--- src/app/tarefas/tarefas.module.ts
+++ src/app/tarefas/tarefas.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { TarefaService } from './shared';
+import { TarefaService, TarefaConcluidaDirective } from './shared';
 import { ListarTarefaComponent } from './listar';
 import { CadastrarTarefaComponent } from './cadastrar';
 import { EditarTarefaComponent } from './editar';
+
 
 
 @NgModule({
   declarations: [
   ListarTarefaComponent,
   CadastrarTarefaComponent,
-  EditarTarefaComponent
+  EditarTarefaComponent,
+  TarefaConcluidaDirective
   ],
 
   imports: [
